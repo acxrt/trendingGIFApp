9073132ae655d04832fdd98994f58ac656302bc5
Aina Cuxart <ainacuxart@gmail.com>
2017-03-23 19:05:27 +0100

    Working on load GIFs

--- trendingGIFApp/TrendingGIFCollectionViewController.swift
+++ trendingGIFApp/TrendingGIFCollectionViewController.swift
@@ -9,14 +9,27 @@
 import Foundation
 import UIKit
 
+import Alamofire
+
+
 class TrendingGIFCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
     var colors = [UIColor.red, UIColor.blue, UIColor.gray, UIColor.green, UIColor.black]
     
     let marginBetweenGifs: CGFloat = 5.0
     
+    var loadedImage: UIImage?
     
-    //MARK: - UICollectionViewDataSource protocol
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let numberOfGifs = 20
+        loadTrendingGifs(limit: numberOfGifs)
+        
+    }
+    
+    
+    // MARK: - UICollectionViewDataSource protocol
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //return gifs.count
         return colors.count
@@ -24,15 +37,21 @@ class TrendingGIFCollectionViewController: UICollectionViewController, UICollect
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell : UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "gifCell", for: indexPath)
+        let cell : gifCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "gifCell", for: indexPath) as! gifCollectionViewCell
         
-        cell.backgroundColor = colors[indexPath.item]
+        
+        if let url = NSURL(string: "http://media0.giphy.com/media/3oKIPkn074os2OhrOw/giphy-downsized.gif") {
+            if let data = NSData(contentsOf: url as URL) {
+                cell.gifImageView.image = UIImage(data: data as Data)
+            }        
+        }
+        
         
         return cell
     }
 
 
-    //MARK: - UICollectionViewDelegate protocol
+    // MARK: - UICollectionViewDelegate protocol
     func collectionView(_ collectionView: UICollectionView,
                             layout collectionViewLayout: UICollectionViewLayout,
                             sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -49,4 +68,23 @@ class TrendingGIFCollectionViewController: UICollectionViewController, UICollect
         
         return UIEdgeInsets(top: marginBetweenGifs, left: marginBetweenGifs, bottom: marginBetweenGifs, right: marginBetweenGifs)
     }
+    
+    
+    // MARK: - Load trending GIFs from API
+    func loadTrendingGifs(limit: Int) {
+        
+        Alamofire.request("http://api.giphy.com/v1/gifs/trending?limit=\(limit)&api_key=dc6zaTOxFJmzC").responseJSON { response in
+//            print(response.request)  // original URL request
+//            print(response.response) // HTTP URL response
+//            print(response.data)     // server data
+//            print(response.result)   // result of response serialization
+//            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+        
+        
+    }
+    
 }
---
+++ trendingGIFApp/gifCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  gifCollectionViewCell.swift
+//  trendingGIFApp
+//
+//  Created by Aina Cuxart on 23/3/17.
+//  Copyright © 2017 Aina Cuxart. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class gifCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var gifImageView: UIImageView!
+    
+}
